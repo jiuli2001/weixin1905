53b325765cbc38eab3b47e849bc57611162f2690
root <root@localhost.localdomain>
2019-12-06 06:38:50 +0000

    test

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('info',function(){
+	phpinfo();
+});
+Route::get('/test/hello','Test\TestController@hello');
--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-//aaaa
+//aaaaaa
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
