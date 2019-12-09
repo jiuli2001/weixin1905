1e27d339c4b895b89455ec97d6484375fde40b41
jiuli <jiuli@localhost.localdomain>
2019-12-09 08:28:31 +0000

    接受推送

--- app/Http/Controllers/Wx/WeixinController.php
+++ app/Http/Controllers/Wx/WeixinController.php
@@ -15,4 +15,32 @@ class WeixinController extends Controller
     {
         echo $request->echostr;
     }
+    public function wechat()
+    {
+        $token = 'echostr';       //开发提前设置好的 token
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $echostr = $_GET["echostr"];
+
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){        //验证通过
+            echo $echostr;
+        }else{
+            die("not ok");
+        }
+    }
+
+    public function receiv(){
+        $log_file = "weixin.log";
+        $data = json_encode($_POST);
+        file_put_contents($log_file,$data,FILE_APPEND);
+    }
+    public function getUserInfo(){
+        $url='ttps://api.weixin.qq.com/cgi-bin/user/info?access_token=ACCESS_TOKEN&openid=OPENID&lang=zh_CN';
+    }
 }
