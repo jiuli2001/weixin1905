d4a73a15575eb09ae790b33d08f1d1958da4944b
jiuli <jiuli@localhost.localdomain>
2019-12-18 18:20:50 +0800

    123

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -32,18 +32,14 @@ class WxController extends Controller
 
     protected function getAccessToken()
     {
-
         $key = 'wx_access_token';
-
         $access_token = Redis::get($key);
         if($access_token){
             return $access_token;
         }
-
         $url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.env('WX_APPID').'&secret='.env('WX_APPSECRET');
         $data_json = file_get_contents($url);
         $arr = json_decode($data_json,true);
-
         Redis::set($key,$arr['access_token']);
         Redis::expire($key,3600);
         return $arr['access_token'];
