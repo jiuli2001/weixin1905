280ed1279abd1c2e88f53ee959c465c81bb8a4ce
jiuli <jiuli@localhost.localdomain>
2019-12-20 14:42:46 +0800

    123

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -5,34 +5,36 @@ namespace App\Http\Controllers\Index;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Model\WxUserModel;
+
 class IndexController extends Controller
 {
-    public function index(){
-        $code=$_GET['code'];
-        $data=$this->getAccessToken($code);
-        //判断用户是否存在
+    public function index()
+    {
+        $code = $_GET['code'];
+        //获取access_token
+        $data = $this->getAccessToken($code);
+
+        // 判断用户是否已经存在
         $openid = $data['openid'];
-        $u=WxUserModel::where(['openid'=>$openid])->first();
-        if ($u){  //用户已存在
-            $user_info=$u->toArray();
+        $u = WxUserModel::where(['openid'=>$openid])->first();
+        if($u){  //用户已存在
+            $user_info = $u->toArray();
         }else{
+            //获取用户信息
             $user_info = $this->getUserInfo($data['access_token'],$data['openid']);
-            //入库
+            // 入库
             WxUserModel::insertGetId($user_info);
         }
 
-        $data=[
-          'u'=>$user_info
+        $data = [
+            'u' => $user_info
         ];
+
         return view('index.index',$data);
     }
 
-
-
-
     /**
-     * 根据code获取access_token
-     * @param $code
+     * 根据code 获取access_token
      */
     protected function getAccessToken($code)
     {
@@ -41,11 +43,8 @@ class IndexController extends Controller
         return json_decode($json_data,true);
     }
 
-
     /**
      * 获取用户基本信息
-     * @param $access_token
-     * @param $openid
      */
     protected function getUserInfo($access_token,$openid)
     {
@@ -53,9 +52,10 @@ class IndexController extends Controller
         $json_data = file_get_contents($url);
         $data = json_decode($json_data,true);
         if(isset($data['errcode'])){
-            // TODO  错误处理
-            die("出错了 40001");       // 40001 标识获取用户信息失败
+            // 错误处理
+            die('嘿!兄弟出错了 40001');  //40001标识获取用户信息失败
         }
-        return $data;           // 返回用户信息
+        return $data;
     }
+
 }
